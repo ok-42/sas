412ec2b58f3225c30d743ffcca95b4d906343856
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2022-12-10 23:27:58 +0600

    Add a function that searches for a text and keeps results

--- utils.sh
+++ utils.sh
@@ -4,3 +4,31 @@ function sm() {
     echo $result
     [ ! -z "$result" ] && echo $result | awk -F':' '{print $1 " -n" $2}' | xargs start notepad++
 }
+
+
+# Search for a given substring or retrieve a search result
+# Takes one argument that should be either a string (to search for) or a number (of a search result)
+function g() {
+
+    # Temporary text file that stores results of grep search
+    GREP_OUTPUT=grep_output.txt
+
+    # https://stackoverflow.com/a/806923
+    REGEXP='^[0-9]+$'
+
+    # String: Search in the current directory and save results to a text file
+    if ! [[ $1 =~ $REGEXP ]] ; then
+       grep -nr --color --exclude-dir=.git --exclude=$GREP_OUTPUT "$1" . > $GREP_OUTPUT
+
+    # Number: Open the n-th line from the previous search results
+    else
+        n=1;
+        while read line; do
+            if [ $n == $1 ]; then
+                echo "$line";
+                SEARCH_RESULT=$line
+            fi
+        n=$((n+1));
+        done < $GREP_OUTPUT;
+    fi
+}
