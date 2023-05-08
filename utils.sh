4810770894befeaabe3bfb120b2debc18612f94f
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2023-05-08 22:40:19 +0600

    Fix ShellCheck warnings

--- utils.sh
+++ utils.sh
@@ -1,8 +1,8 @@
 # Find a macro definition and open it in Notepad++
 function sm() {
     result=$(grep -nr "%macro $1" .)
-    echo $result
-    [ ! -z "$result" ] && echo $result | awk -F':' '{print $1 " -n" $2}' | xargs start notepad++
+    echo "$result"
+    [ -n "$result" ] && echo "$result" | awk -F':' '{print $1 " -n" $2}' | xargs start notepad++
 }
 
 
